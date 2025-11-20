62a2a278f39b276e331687cf73a01b0cd126359a
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-11-20 22:37:23 +0500

    INFRA: Initialize API Project

---
+++ UzStay.Api/Startup.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace UzStay.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>        
+            Configuration = configuration;
+
+
+        public IConfiguration Configuration { get; }
+
+        OpenApiInfo apiInfo = new OpenApiInfo { Title = "UzStay.Api", Version = "v1" };
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc(
+                    name: "v1", 
+                    info : apiInfo); 
+            });
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(option => option.SwaggerEndpoint(
+                    url: "/swagger/v1/swagger.json", 
+                    name: "UzStay.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+                endpoints.MapControllers());
+        }
+    }
+}
