cc5895fecc0de4b747ebee1d82bede4d52062458
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-11-22 22:35:15 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync -> WIP

---
+++ UzStay.Api/Models/Foundations/Guests/Exception/GuestValidationException.cs
@@ -0,0 +1,13 @@
+﻿using Xeptions;
+
+namespace UzStay.Api.Models.Foundations.Guests.Exception
+{
+    public class GuestValidationException : Xeption
+    {
+        public GuestValidationException(Xeption innerException)
+            :base(message: "Guest validation error occurred, fix the errors and try again",
+                 innerException)
+        { }
+    }
+}
+
---
+++ UzStay.Api/Models/Foundations/Guests/Exception/NullGuestException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace UzStay.Api.Models.Foundations.Guests.Exception
+{
+    public class NullGuestException : Xeption
+    {
+        public NullGuestException() 
+            :base(message: "Guest is null")
+        { }
+    }
+}
