195750b2259007cd7e3f014941ec94bf51c1135f
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-11-22 23:14:41 +0500

    CODE RUB: Add Logging Broker

--- UzStay.Api/Services/Foundations/Guests/GuestService.cs
+++ UzStay.Api/Services/Foundations/Guests/GuestService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using UzStay.Api.Brokers.Logging;
 using UzStay.Api.Brokers.Storages;
 using UzStay.Api.Models.Foundations.Guests;
 
@@ -7,9 +8,13 @@ namespace UzStay.Api.Services.Foundations.Guests
     public class GuestService : IGuestService
     {
         private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
 
-        public GuestService(IStorageBroker storageBroker) =>
+        public GuestService(IStorageBroker storageBroker, ILoggingBroker loggingBroker)
+        {
             this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
 
         public async ValueTask<Guest> AddGuestAsync(Guest guest) =>
             await this.storageBroker.InsertGuestsAsync(guest);
