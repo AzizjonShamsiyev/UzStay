b6a26a2aa4e73282edf3dd33b974cfda36f8961d
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-11-22 22:35:15 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync -> WIP

---
+++ UzStay.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Validations.Add.cs
@@ -0,0 +1,27 @@
+﻿using UzStay.Api.Models.Foundations.Guests;
+using UzStay.Api.Models.Foundations.Guests.Exception;
+using Xunit;
+
+namespace UzStay.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync()
+        {
+            //given
+            Guest nullGuest = null;
+            var nullGuestException = new NullGuestException();
+
+            var expectedGuestException =
+                new GuestValidationException(nullGuestException);
+
+            //when
+            ValueTask<Guest> addGuestTask = this.guestSevice.AddGuestAsync(nullGuest);
+
+            //then
+            await Assert.ThrowsAsync<GuestValidationException>(() =>
+                addGuestTask.AsTask());
+        }
+    }
+}
