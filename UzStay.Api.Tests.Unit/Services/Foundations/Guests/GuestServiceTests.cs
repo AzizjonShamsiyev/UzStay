47abff4bba9c5fd316180a3ed36c06036aabfe9e
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-11-22 13:56:35 +0500

    ShouldAddAsync -> FAIL

---
+++ UzStay.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using Tynamix.ObjectFiller;
+using UzStay.Api.Brokers.Storages;
+using UzStay.Api.Models.Foundations.Guests;
+using UzStay.Api.Services.Foundations.Guests;
+
+namespace UzStay.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly IGuestService guestSevice;
+
+        public GuestServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.guestSevice =
+                new GuestService(storageBroker: this.storageBrokerMock.Object);
+        }
+
+        private static Guest CreateRandomGuest() =>
+            CreateGuestFiller(date: GetRandomDateTimeOffset()).Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Filler<Guest> CreateGuestFiller(DateTimeOffset date)
+        {
+            var filler = new Filler<Guest>();
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(date);
+
+            return filler;
+        }
+    }
+}
---
+++ UzStay.Api/Services/Foundations/Guests/GuestService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using UzStay.Api.Brokers.Storages;
+using UzStay.Api.Models.Foundations.Guests;
+
+namespace UzStay.Api.Services.Foundations.Guests
+{
+    public class GuestService : IGuestService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public GuestService(IStorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+        
+
+        public ValueTask<Guest> AddGuestAsync(Guest guest) =>
+            throw new System.NotImplementedException();
+
+    }
+}
