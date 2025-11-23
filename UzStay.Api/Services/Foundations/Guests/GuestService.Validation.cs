b2820c112983a30e21892bc49d341bc69b077fcb
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-11-23 09:22:44 +0500

    CODE RUB: Implement TryCatch & Proper Validation

---
+++ UzStay.Api/Services/Foundations/Guests/GuestService.Validation.cs
@@ -0,0 +1,14 @@
+﻿using UzStay.Api.Models.Foundations.Guests;
+using UzStay.Api.Models.Foundations.Guests.Exception;
+
+namespace UzStay.Api.Services.Foundations.Guests
+{
+    public partial class GuestService
+    {
+        private void ValidateGuestNotNull(Guest guest)
+        {
+            if (guest is null)
+                throw new NullGuestException();
+        }
+    }
+}
