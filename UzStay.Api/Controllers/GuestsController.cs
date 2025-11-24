cfe953396c95746daa4f43bbb870b781b81e91fd
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-11-24 19:31:48 +0500

    EXPOSERS: Post Controller

---
+++ UzStay.Api/Controllers/GuestsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using System.Threading.Tasks;
+using UzStay.Api.Models.Foundations.Guests;
+using UzStay.Api.Models.Foundations.Guests.Exceptions;
+using UzStay.Api.Services.Foundations.Guests;
+
+namespace UzStay.Api.Controllers
+{
+    [ApiController]
+    [Route("api / [controller]")]
+    public class GuestsController : RESTFulController
+    {
+        private readonly IGuestService guestService;
+
+        public GuestsController(IGuestService guestService)
+        {
+            this.guestService = guestService;
+        }
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Guest>> PostGuestAsync(Guest guest)
+        {
+            try
+            {
+                Guest postedGuest = 
+                    await this.guestService.AddGuestAsync(guest);
+                return Created(postedGuest);
+            }
+            catch(GuestValidationException guestValidationException)
+            {
+                return BadRequest(guestValidationException.InnerException);
+            }
+		    catch (GuestDependencyValidationException guestDependencyValidationException)
+			    when(guestDependencyValidationException.InnerException is AlreadyExistGuestException)
+            {
+                return Conflict(guestDependencyValidationException.InnerException);
+            }
+            catch (GuestDependencyValidationException guestDependencyValidationException)
+            {
+                return BadRequest(guestDependencyValidationException.InnerException);
+            }
+            catch(GuestDependencyException guestDependencyException)
+            {
+                return InternalServerError(guestDependencyException.InnerException);
+            }
+            catch(GuestServiceException guestServiceException)
+            {
+                return InternalServerError(guestServiceException.InnerException);
+            }
+        }
+    }
+}
