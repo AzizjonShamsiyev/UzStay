4ab4e373c5d20d9ac57e8b4912fd88e09cbb8905
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-11-24 16:56:24 +0500

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ UzStay.Api/Models/Foundations/Guests/Exceptions/FailedGuestServiceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xeptions;
+
+namespace UzStay.Api.Models.Foundations.Guests.Exceptions
+{
+    public class FailedGuestServiceException : Xeption
+    {
+        public FailedGuestServiceException(Exception innerException)
+            :base(message: "Failed guest service error occurred, contact support",
+                 innerException)
+        { }
+    }
+}
