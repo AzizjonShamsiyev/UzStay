823c87549b0a5a0c0f117589b9b96baa18f23e08
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-11-25 08:31:43 +0500

    ShouldRetrieveAllGuests -> FAIL

---
+++ UzStay.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Moq;
+using UzStay.Api.Models.Foundations.Guests;
+using Xunit;
+
+namespace UzStay.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public void ShouldRetrieveAllGuests()
+        {
+            //given
+            IQueryable<Guest> randomGuests = CreateRandomGuests();
+            IQueryable<Guest> storageGuests = randomGuests;
+            IQueryable<Guest> expectedGuests = storageGuests;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllGuests())
+                    .Returns(storageGuests);
+
+            //when
+            IQueryable<Guest> actualGuests = 
+                this.guestService.RetrieveAllGuests();
+
+            //then
+            actualGuests.Should().BeEquivalentTo(expectedGuests);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllGuests(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
